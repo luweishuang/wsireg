905571fb2e3d805315d166c093b26c103291ac7a
luweishuang <pifuchan@gmail.com>
2022-03-17 10:22:59 +0800

    self5

--- wsireg/demo_self5.py
+++ wsireg/demo_self5.py
@@ -9,9 +9,7 @@ def bilinear_interpolation_of_patch_registration(master_srcdata, target_srcdata)
     print("Beginning bilinear_interpolation_of_patch_registration...")
     w_shape = (1000, 1000, 4)    # window_size
     w_step = (500, 500, 4)       # 步长
-    padding = w_step[0]
-    # master_data = cv2.copyMakeBorder(master_srcdata, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=(255, 255, 255))
-    # target_data = cv2.copyMakeBorder(target_srcdata, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+    padding = w_step[0]          # must do step padding
     master_data = cv2.copyMakeBorder(master_srcdata, padding, padding, padding, padding, cv2.BORDER_REFLECT)
     target_data = cv2.copyMakeBorder(target_srcdata, padding, padding, padding, padding, cv2.BORDER_REFLECT)
     master_img = cv2.cvtColor(master_data, code=cv2.COLOR_BGRA2RGBA)
@@ -41,8 +39,8 @@ def bilinear_interpolation_of_patch_registration(master_srcdata, target_srcdata)
     quilts = bilinear.quilter(map_patches)
     wquilts = bilinear.bilinear_wquilts(map_patches)
     qmaps = [q * w for q, w in zip(quilts, wquilts)]
-    tt = qmaps[0] + qmaps[1] + qmaps[2] + qmaps[3]
-    print("tt.shape=", tt.shape)
+    # tt = qmaps[0] + qmaps[1] + qmaps[2] + qmaps[3]
+    # print("tt.shape=", tt.shape)
     summed = (qmaps[0] + qmaps[1] + qmaps[2] + qmaps[3]).reshape(2, 3000, 5000).astype(np.float32)
 
     master_remap = cv2.remap(master_img, summed[0], summed[1], interpolation=cv2.INTER_LINEAR)    # summed 是坐标映射关系
@@ -67,12 +65,7 @@ def draw_img():
     cv2.imwrite("master_data.jpg", master_data)
 
 
-def pad_imgs(master_srcdata, target_srcdata):
-    master3 = master_srcdata[4050:4850,:,:]
-    # cv2.imwrite("master3.jpg", master3)
-    target3 = target_srcdata[4470:5270,:,:]
-    # cv2.imwrite("target3.jpg", target3)
-
+def pad_imgs(master3, target3):
     master_h, master_w, _ = master3.shape
     target_h, target_w, _ = target3.shape
     assert master_h == target_h and master_w == target_w
@@ -91,6 +84,73 @@ def pad_imgs(master_srcdata, target_srcdata):
     return master3_pad, target3_pad, top_pad, down_pad, left_pad, right_pad
 
 
+MAX_FEATURES = 5000
+GOOD_MATCH_PERCENT = 0.45
+def alignImages_Perspective(img1, img2):
+    im1Gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    im2Gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = list(matcher.match(descriptors1, descriptors2, None))
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+
+    height, width, channels = img2.shape
+    # Perspective
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+    im1Reg_Perspective = cv2.warpPerspective(img1, h, (width, height))   # 透视变换
+    return im1Reg_Perspective
+
+
+def process_single_imgpart(img_master, target_img):
+    master_height, master_width, _ = img_master.shape
+    cur_height, cur_width, _ = target_img.shape
+    assert cur_width == master_width
+    top_pad, down_pad = 0, 0
+    target_imgpad = target_img.copy()
+    if master_height > cur_height:
+        top_pad = int((master_height - cur_height)/2)
+        down_pad = master_height - cur_height - top_pad
+        target_imgpad = cv2.copyMakeBorder(target_img, top_pad, down_pad, 0, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+    elif master_height < cur_height:
+        print("cur_height > master_height", cur_height, master_height)
+    img_show = target_imgpad.copy()
+    im2Gray = cv2.cvtColor(target_imgpad, cv2.COLOR_BGR2GRAY)
+
+    im1Reg_Perspective = alignImages_Perspective(img_master, target_imgpad)
+
+    imRegGray = cv2.cvtColor(im1Reg_Perspective, cv2.COLOR_BGR2GRAY)
+    diff = cv2.absdiff(imRegGray, im2Gray)
+    # cv2.imwrite("diff.jpg", diff)
+    ret, thresh = cv2.threshold(diff, 120, 255, cv2.THRESH_BINARY)   # 120
+    # cv2.imwrite("thresh.jpg", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 1 and area < max(cur_height, cur_width):
+            cv2.drawContours(img_show, cnt, -1, (0, 0, 255), 2)
+
+    img_out = img_show[top_pad: master_height - down_pad, :, : ]
+    return img_out
+
 
 if __name__ == "__main__":
     # draw_img()
@@ -99,10 +159,26 @@ if __name__ == "__main__":
     root = "../data/"
     master_srcdata = cv2.imread(root + "OK1_1.jpg")
     target_srcdata = cv2.imread(root + "NG1_1.jpg")
-    master3_pad, target3_pad, top_pad, down_pad, left_pad, right_pad = pad_imgs(master_srcdata, target_srcdata)
+    master3 = master_srcdata[4050:4850,:,:]
+    # cv2.imwrite("master3.jpg", master3)
+    target3 = target_srcdata[4470:5270,:,:]
+    # cv2.imwrite("target3.jpg", target3)
+
+    # padding to 1000s, at least 2000
+    master3_pad, target3_pad, top_pad, down_pad, left_pad, right_pad = pad_imgs(master3, target3)
     # cv2.imwrite("master3_pad.jpg", master3_pad)
     # cv2.imwrite("target3_pad.jpg", target3_pad)
-    # exit()
 
+    masterpad_h, masterpad_w, _ = master3_pad.shape
     master_reg_pad = bilinear_interpolation_of_patch_registration(master3_pad, target3_pad)
-    master_reg = master_reg_pad[top_pad: master_height - down_pad, :, : ]
+    master3_reg = master_reg_pad[top_pad: masterpad_h-down_pad, left_pad:masterpad_w-right_pad, : ]
+    cv2.imwrite("master3_reg.jpg", master3_reg)
+    cv2.imwrite("master3.jpg", master3)
+    cv2.imwrite("target3.jpg", target3)
+
+    # Stage Five: high-precision feature alignment
+    master_reg_out = process_single_imgpart(master3_reg, target3)
+    cv2.imwrite("master_reg_out.jpg", master_reg_out)
+
+    master_out = process_single_imgpart(master3, target3)
+    cv2.imwrite("master_out.jpg", master_out)
